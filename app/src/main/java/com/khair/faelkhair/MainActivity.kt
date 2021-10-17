21eea2c3c74765380550c9544e2118d3d64dfb2a
ahmedmuhammed <ahmedmuhammedpro@gmail.com>
2021-10-17 06:49:28 +0200

    add mvvm layer, screens demos

---
+++ app/src/main/java/com/khair/faelkhair/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.khair.faelkhair
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.khair.faelkhair.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = findNavController(R.id.main_nav_host)
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+}
